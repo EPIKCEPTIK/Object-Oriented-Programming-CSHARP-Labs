7abf9afde7dc24dab6295d66ff31805b68c93520
EPIKCEPTIK <vitaliishm09@gmail.com>
2025-05-04 12:54:28 +0300

    Lb2

---
+++ LabWorks/Lab2/Program.cs
@@ -0,0 +1,53 @@
+﻿
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lab2
+{
+    internal class Program
+    {
+        static void Main()
+        {
+            
+            List<User> users = new List<User>
+                {
+                    new Admin { UserName = "AdminUser", Email = "admin@example.com" },
+                    new Moderator { UserName = "ModUser", Email = "mod@example.com" },
+                    new RegularUser { UserName = "RegUser", Email = "user@example.com" },
+                };
+
+
+            foreach (User user in users)
+            {
+                user.DisplayInfo();
+            }
+            foreach (var user in users)
+            {
+                user.SetPassword("123");
+            }
+            foreach (var user in users)
+            {
+                user.Authenticate("1234");
+            }
+            foreach (User user in users)
+            {
+                if (user is Admin admin)
+                {
+                    admin.BlockUser(new RegularUser { UserName = "AnotherUser", Email = "another@example.com" });
+                }
+                else if (user is Moderator moderator)
+                {
+                    moderator.ModerateContent();
+                }
+                else if (user is RegularUser regularUser)
+                {
+                    regularUser.PostComment();
+                }
+            }
+
+        }
+    }
+}
---
+++ LabWorks/Lab2/User.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml.Linq;
+
+namespace Lab2
+{
+    internal class User
+    {
+        public required string UserName { get; set; }
+        public required string Email { get; set; }
+        private string? _password;
+        public void SetPassword(string newPassword)
+        {
+            _password = newPassword;
+        }
+        public bool Authenticate(string inputPassword)
+        {
+            if (_password != inputPassword) {
+                Console.WriteLine("Вхід не вдався");
+                return false;
+            }
+            Console.WriteLine("Вхід успішний");
+            return true;
+        }
+        public virtual void DisplayInfo()
+        {
+            Console.WriteLine("Ім'я: " + UserName + " Email: " + Email + " Роль: Користувач");
+        }
+
+    }
+    class Admin : User
+    {
+        public void BlockUser(User user)
+        {
+            Console.WriteLine("Користувача " + user.UserName + " заблоковано");
+        }
+        public override void DisplayInfo() {
+            Console.WriteLine("Ім'я: " + UserName + " Email: " + Email + " Роль: Адміністратор");
+        }
+    }
+    class Moderator : User
+    {
+        public void ModerateContent()
+        {
+            Console.WriteLine("Контент модеровано");
+        }
+        public override void DisplayInfo()
+        {
+            Console.WriteLine("Ім'я: " + UserName + " Email: " + Email + " Роль: Модератор");
+
+        }
+    }
+    class RegularUser : User
+    {
+        public void PostComment()
+        {
+            Console.WriteLine("Коментарь опубліковано");
+
+        }
+        public override void DisplayInfo()
+        {
+            Console.WriteLine("Ім'я: " + UserName + " Email: " + Email + " Роль: Звичайний користувач");
+
+        }
+    }
+}
